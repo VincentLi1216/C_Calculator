6ac9e29bd451afef4ab6d5a73d68d59b98a46fd0
VincentLi1216 <sunnus.tw@gmail.com>
2023-04-17 14:48:48 +0800

    second commit

---
+++ infix2Postfix.c
@@ -0,0 +1,192 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXSIZE 1000000 // 設定最大的字串長度
+
+char token[MAXSIZE];  // 輸入的中序運算式
+char output[MAXSIZE]; // 轉換後的後序運算式
+
+int op_hierarchy(char op); // 回傳運算子的優先權
+void append(char op);      // 將運算子加入後序運算式的字串尾端
+
+typedef struct
+{
+  char arr[MAXSIZE];
+  int top;
+} Stack;
+
+Stack *createStack() // 創建 stack
+{
+  Stack *stack = (Stack *)malloc(sizeof(Stack));
+  stack->top = -1;
+  return stack;
+}
+
+void push(Stack *stack, char data) // 壓入元素到 stack 中
+{
+  if (stack->top == MAXSIZE - 1)
+  {
+    printf("Error: Stack overflow\n");
+    return;
+  }
+  stack->arr[++stack->top] = data;
+}
+
+char pop(Stack *stack) // 彈出 stack 中的元素
+{
+  if (stack->top == -1)
+  {
+    printf("Error: Stack underflow\n");
+    return '\0';
+  }
+  char data = stack->arr[stack->top--];
+  return data;
+}
+
+char top(Stack *stack) // 回傳 stack 頂端的元素
+{
+  if (stack->top == -1)
+  {
+    printf("Error: Stack is empty\n");
+    return '\0';
+  }
+  return stack->arr[stack->top];
+}
+
+int isEmpty(Stack *stack) // 檢查 stack 是否為空
+{
+  return stack->top == -1;
+}
+
+int isFull(Stack *stack) // 檢查 stack 是否已滿
+{
+  return stack->top == MAXSIZE - 1;
+}
+
+void display(Stack *stack) // 顯示 stack 中的元素
+{
+  if (stack->top == -1)
+  {
+    printf("Stack is empty\n");
+    return;
+  }
+  printf("Stack: ");
+  for (int i = stack->top; i >= 0; i--)
+  {
+    printf("%c ", stack->arr[i]);
+  }
+  printf("\n");
+}
+
+void infix2Postfix()
+{
+  char nowchar;
+  Stack *opStack = createStack(); // 創建 stack
+
+  for (int i = 0; i < strlen(token); i++)
+  {
+    nowchar = token[i];
+    switch (op_hierarchy(nowchar))
+    {
+    case 0: // 運算元直接加入後序運算式
+      append(nowchar);
+      break;
+    case 1: // 加減運算子的處理
+      while (!isEmpty(opStack) && op_hierarchy(top(opStack)) >= op_hierarchy(nowchar) && top(opStack) != '(')
+      {
+        append(pop(opStack));
+      }
+      push(opStack, nowchar);
+      break;
+    case 2: // 乘除餘運算子的處理
+      while (!isEmpty(opStack) && op_hierarchy(top(opStack)) >= op_hierarchy(nowchar) && top(opStack) != '(')
+      {
+        append(pop(opStack));
+      }
+      push(opStack, nowchar);
+      break;
+    case 3: // 乘除餘運算子的處理
+      while (!isEmpty(opStack) && op_hierarchy(top(opStack)) >= op_hierarchy(nowchar) && top(opStack) != '(')
+      {
+        append(pop(opStack));
+      }
+      push(opStack, nowchar);
+      break;
+    case 4: // 左括號的處理
+      push(opStack, nowchar);
+      break;
+    case 5: // 右括號的處理
+      while (!isEmpty(opStack) && top(opStack) != '(')
+      {
+        append(pop(opStack));
+      }
+      if (!isEmpty(opStack) && top(opStack) == '(')
+      {
+        pop(opStack);
+      }
+      else
+      {
+        printf("Error: Mismatched parentheses\n");
+        return;
+      }
+      break;
+    default:
+      printf("Error: Invalid input\n");
+      return;
+    }
+  }
+
+  // 把 stack 中剩下的運算子加入後序運算式
+  while (!isEmpty(opStack))
+  {
+    if (top(opStack) == '(' || top(opStack) == ')')
+    {
+      printf("Error: Mismatched parentheses\n");
+      return;
+    }
+    append(pop(opStack));
+  }
+}
+
+int op_hierarchy(char op)
+{
+  switch (op)
+  {
+  case '+':
+  case '-':
+    return 1;
+  case '*':
+  case '/':
+  case '%':
+    return 2;
+  case '^':
+    return 3;
+  case '(':
+    return 4;
+  case ')':
+    return 5;
+  default:
+    return 0;
+  }
+}
+
+void append(char op)
+{
+  if (op == ' ' || op == '\n' || op == '\0')
+  {
+    return;
+  }
+  int len = strlen(output);
+  output[len] = op;
+  output[len + 1] = '\0';
+}
+
+int main()
+{
+  printf("Please enter an infix expression: ");
+  fgets(token, MAXSIZE, stdin);
+  infix2Postfix();
+  printf("Postfix expression: %s\n", output);
+  return 0;
+}
